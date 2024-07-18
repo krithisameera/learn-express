a804682474d5d52b36a6f14c9f941b3b5e844a47
krithisameera <vadrevu.krithi@gmail.com>
2024-07-18 19:00:24 +0200

    response

--- myApp.js
+++ myApp.js
@@ -1,23 +1,41 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
+//absolutePath = __dirname + '/views/index.html'
+
 
 console.log("Hello World")
 
+/* app.get('/', function(req,res) {
+    res.send("Hello Express");
+}) */
+
+app.get('/', function(req,res) {//route handler
+    res.sendFile( __dirname + '/views/index.html');
+});
+
+app.use('/public', express.static(__dirname + '/public'));//middleware function express.static
+
+
+/* app.get('/json', (req, res) => {
+    res.json({
+        message:"Hello json"
+    });
+}); */
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE == "uppercase") {
+        res.json({
+            message:"HELLO JSON"
+        });
+    } else {
+        res.json({
+            message:"Hello json"
+        });
+    }
+});
 
 
 
