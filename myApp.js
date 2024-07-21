a10d19fbdeb5578c0565cd3305851fab8c6630eb
krithisameera <vadrevu.krithi@gmail.com>
2024-07-21 13:42:31 +0200

    add post

--- myApp.js
+++ myApp.js
@@ -1,21 +1,28 @@
+const bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 require('dotenv').config()
 //absolutePath = __dirname + '/views/index.html'
-
-
 console.log("Hello World")
 
 /* app.get('/', function(req,res) {
     res.send("Hello Express");
 }) */
 
-app.get('/', function(req,res) {//route handler
+
+/* app.use( function(req, res, next) {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});    
+ */
+app.use('/', bodyParser.urlencoded({extended: false}));
+//route handler
+app.get('/', function(req,res) {
     res.sendFile( __dirname + '/views/index.html');
 });
 
-app.use('/public', express.static(__dirname + '/public'));//middleware function express.static
-
+//middleware function express.static
+app.use('/public', express.static(__dirname + '/public'));
 
 /* app.get('/json', (req, res) => {
     res.json({
@@ -23,9 +30,7 @@ app.use('/public', express.static(__dirname + '/public'));//middleware function 
     });
 }); */
 
-
-
-app.get('/json', (req, res) => {
+/* app.get('/json', (req, res) => {
     if (process.env.MESSAGE_STYLE == "uppercase") {
         res.json({
             message:"HELLO JSON"
@@ -35,14 +40,39 @@ app.get('/json', (req, res) => {
             message:"Hello json"
         });
     }
+}); */
+
+/* app.get('/now', function(req, res, next){
+    req.time = new Date().toString();
+    next();
+}, function(req, res){
+    res.json({time: req.time});
+}); */
+
+/* app.get('/:word/echo', function(req, res){
+    const {word} = req.params;
+    res.json({echo: word});
+}); */
+
+/* app.get("/name", (req,res) => {
+    const firstname = req.query.first;
+    const lastname = req.query.last;      
+
+    //const fullname = `${firstname} ${lastname}`
+    res.json({
+    name: `${firstname} ${lastname}`
+    });
+    
+}); */
+    
+app.post('/name', (req, res) => {
+    const firstname = req.body.first;
+    const lastname = req.body.last;
+
+    res.json({
+        name: `${firstname} ${lastname}`
+    });
 });
-
-
-
-
-
-
-
 
 
 
